99ed6b2b2571e07e6d0e945544203c6be20a30a0
Dmitry G. Anderson <dandgerson@gmail.com>
2022-12-31 16:42:18 +0300

    add map data & animal & breed

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,7 +1,13 @@
 import { useState } from "react";
+const animals = ["bird", "dog", "cat", "reptile", "rabbit"];
 
 export const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
+  const [animal, setAnimal] = useState("");
+  const breeds = [];
+
+  const [breed, setBreed] = useState("");
+
   console.log("render");
   return (
     <div className="search-params">
@@ -14,6 +20,44 @@ export const SearchParams = () => {
             onChange={(e) => setLocation(e.target.value)}
             placeholder="Location"
           />
+        </label>
+
+        <label htmlFor="animal">
+          Animal
+          <select
+            name="animal"
+            id="animal"
+            onChange={(e) => {
+              setAnimal(e.target.value);
+              setBreed("");
+            }}
+            value={animal}
+          >
+            <option />
+            {animals.map((animalName) => (
+              <option key={animalName} value={animalName}>
+                {animalName}
+              </option>
+            ))}
+          </select>
+        </label>
+
+        <label htmlFor="breed">
+          Breeds
+          <select
+            name="breed"
+            id="breed"
+            onChange={(e) => setBreed(e.target.value)}
+            value={breed}
+            disabled={breeds.length === 0}
+          >
+            <option />
+            {breeds.map((breedName) => (
+              <option key={breedName} value={breedName}>
+                {breedName}
+              </option>
+            ))}
+          </select>
         </label>
         <button>Submit</button>
       </form>
