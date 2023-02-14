6062c4df59a5599068cf2769d5aec3c9c481eb99
Dmitry G. Anderson <dandgerson@gmail.com>
2023-02-14 21:42:40 +0300

    styling the Pet component

--- src/Pet.jsx
+++ src/Pet.jsx
@@ -1,7 +1,15 @@
-export const Pet = ({ name, animal, breed }) => (
-  <div>
-    <h1>{name}</h1>
-    <h2>{animal}</h2>
-    <h2>{breed}</h2>
-  </div>
-);
+export const Pet = ({ name, animal, breed, location, id, images }) => {
+  const hero = "http://pets-images.dev-apis.com/pets/none.jpg";
+
+  return (
+    <a href={`/details/${id}`} className="pet">
+      <div className="image-container">
+        <img src={images.length ? images[0] : hero} alt={name} />
+      </div>
+      <div className="info">
+        <h1>{name}</h1>
+        <h2>{`${animal} - ${breed} - ${location}`}</h2>
+      </div>
+    </a>
+  );
+};
