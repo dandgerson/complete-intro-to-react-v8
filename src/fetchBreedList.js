619a5320adfea752d5cd072cfbe1045290ccdbc7
Dmitry G. Anderson <dandgerson@gmail.com>
2023-02-20 01:17:34 +0300

    refactoring fetchBreedList

---
+++ src/fetchBreedList.js
@@ -0,0 +1,17 @@
+export const fetchBreedList = async ({ queryKey }) => {
+  const [, animal] = queryKey;
+
+  if (!animal) {
+    return [];
+  }
+
+  const apiResponse = await fetch(
+    `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
+  );
+
+  if (!apiResponse.ok) {
+    throw new Error(`breeds/${animal} fetch is not ok`);
+  }
+
+  return apiResponse.json();
+};
--- src/useBreedList.js
+++ src/useBreedList.js
@@ -1,32 +1,8 @@
-import { useEffect, useState } from "react";
-const localCache = {};
+import { useQuery } from "@tanstack/react-query";
+import { fetchBreedList } from "./fetchBreedList";
 
 export const useBreedList = (animal) => {
-  const [status, setStatus] = useState("unloaded");
-  const [breedList, setBreedList] = useState([]);
+  const results = useQuery(["breeds", animal], fetchBreedList);
 
-  useEffect(() => {
-    const requestBreeds = async () => {
-      setBreedList([]);
-      setStatus("loading");
-
-      const res = await fetch(
-        `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
-      );
-      const json = await res.json();
-      localCache[animal] = json.breeds || [];
-      setBreedList(localCache[animal]);
-      setStatus("loaded");
-    };
-
-    if (!animal) {
-      setBreedList([]);
-    } else if (localCache[animal]) {
-      setBreedList(localCache[animal]);
-    } else {
-      requestBreeds();
-    }
-  }, [animal]);
-
-  return [breedList, status];
+  return [results?.data?.breeds ?? [], status];
 };
