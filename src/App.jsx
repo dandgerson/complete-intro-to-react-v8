48748ca295fed36dbf29d8c733ebbab8cc457bc9
Dmitry G. Anderson <dandgerson@gmail.com>
2023-02-20 00:06:21 +0300

    useParams

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import { SearchParams } from "./SearchParams";
 import { Details } from "./Details";
 
 export const App = () => {
   return (
     <BrowserRouter>
-      <h1>Adopt me!</h1>
+      <header>
+        <Link to="/">Adopt me!</Link>
+      </header>
       <Routes>
         <Route path="/details/:id" element={<Details />} />
         <Route path="/" element={<SearchParams />} />
--- src/Details.jsx
+++ src/Details.jsx
@@ -1,1 +1,6 @@
-export const Details = () => <h2>Details</h2>;
+import { useParams } from "react-router-dom";
+
+export const Details = () => {
+  const { id } = useParams();
+  return <h2>Details {id}</h2>;
+};
