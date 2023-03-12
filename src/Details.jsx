c8fbb8ab4791618a7c495aec3296bc0e2aed797a
Dmitry G. Anderson <dandgerson@gmail.com>
2023-03-13 00:02:40 +0300

    implementing modals with portals

--- src/Details.jsx
+++ src/Details.jsx
@@ -3,10 +3,13 @@ import { Link, useParams } from "react-router-dom";
 import { fetchPets } from "./fetchPets";
 import { Carousel } from "./Caruosel";
 import { ErrorBoundary } from "./ErrorBoundary";
+import { useState } from "react";
+import { Modal } from "./Modal";
 
 export const Details = () => {
   const { id } = useParams();
   const results = useQuery(["details", id], fetchPets);
+  const [isModalShown, setIsModalShown] = useState(false);
 
   if (results.isError) {
     return <h2>ohno!!!</h2>;
@@ -28,8 +31,28 @@ export const Details = () => {
       <div>
         <h1>{pet.name}</h1>
         <h2>{`${pet.animal} - ${pet.breed} - ${pet.city}, ${pet.state}`}</h2>
-        <button>{`Adopt ${pet.name}`}</button>
+        <button
+          onClick={() => setIsModalShown(true)}
+        >{`Adopt ${pet.name}`}</button>
         <p>{pet.description}</p>
+
+        {isModalShown ? (
+          <Modal
+            renderContent={() => {
+              return (
+                <div>
+                  <h1>Would you like to adopt {pet.name}?</h1>
+                  <div className="buttons">
+                    <button onClick={() => setIsModalShown(false)}>Yes</button>
+                    <button onClick={() => setIsModalShown(false)}>No</button>
+                  </div>
+                </div>
+              );
+            }}
+            target="#modal"
+            onCancel={() => setIsModalShown(false)}
+          />
+        ) : null}
       </div>
     </div>
   );
--- src/Modal.jsx
+++ src/Modal.jsx
@@ -1,19 +1,32 @@
-import { useEffect, useRef } from "react";
+import { useCallback, useEffect, useRef } from "react";
 import { createPortal } from "react-dom";
 
-export const Modal = ({ renderContent, target }) => {
+export const Modal = ({ renderContent, target, onCancel = () => {} }) => {
   const elRef = useRef(null);
 
   if (!elRef.current) {
     elRef.current = document.createElement("div");
   }
 
+  const handleOnCancel = useCallback(
+    (e) => {
+      if (e.target === document.querySelector(target)) {
+        onCancel();
+      }
+    },
+    [onCancel, target]
+  );
+
   useEffect(() => {
     const modalRoot = document.querySelector(target);
     modalRoot.appendChild(elRef.current);
+    modalRoot.addEventListener("click", handleOnCancel);
 
-    return () => modalRoot.removeChild(elRef.current);
-  }, [target]);
+    return () => {
+      modalRoot.removeChild(elRef.current);
+      modalRoot.removeEventListener("click", handleOnCancel);
+    };
+  }, [target, onCancel, handleOnCancel]);
 
   return createPortal(<div>{renderContent()}</div>, elRef.current);
 };
