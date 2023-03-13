56eb128a337175694c22ac4cdcbaf5ad636090fb
Dmitry G. Anderson <dandgerson@gmail.com>
2023-03-13 17:58:09 +0300

    context

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import { SearchParams } from "./SearchParams";
 import { DetailsWithErrorBoundary } from "./Details";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useState } from "react";
+import { AdoptedPetContext } from "./AdoptedPetContext";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -13,17 +15,21 @@ const queryClient = new QueryClient({
 });
 
 export const App = () => {
+  const [adoptedPet] = useState(null);
+
   return (
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
-        <header>
-          <Link to="/">Adopt me!</Link>
-        </header>
+        <AdoptedPetContext.Provider value={adoptedPet}>
+          <header>
+            <Link to="/">Adopt me!</Link>
+          </header>
 
-        <Routes>
-          <Route path="/details/:id" element={<DetailsWithErrorBoundary />} />
-          <Route path="/" element={<SearchParams />} />
-        </Routes>
+          <Routes>
+            <Route path="/details/:id" element={<DetailsWithErrorBoundary />} />
+            <Route path="/" element={<SearchParams />} />
+          </Routes>
+        </AdoptedPetContext.Provider>
       </QueryClientProvider>
     </BrowserRouter>
   );
